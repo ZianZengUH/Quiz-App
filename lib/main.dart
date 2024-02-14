02c75507154e3ebc319ca860a78e66251df73700
github_username <feiyi@hawaii.edu>
2024-02-14 11:56:59 -1000

    BLE Scan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/bluetooth_controller.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +26,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -31,76 +35,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0; // Initially selected index
+
   final TextEditingController _answerController = TextEditingController();
-  // Define the default question as a state variable
   final String _defaultQuestion = 'What is the most efficient algorithm for finding a chosen number within 100?';
 
   void _submitAnswer() {
     // The submit logic here
   }
 
+  // Function to navigate to the search page
+  void _navigateToSearch() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => SearchPage()));
+  }
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = TextStyle(
       fontSize: 18, 
-      fontWeight: FontWeight.bold, // Use FontWeight.bold for bold text
+      fontWeight: FontWeight.bold,
     );
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        reverse: true, // Ensures that the view scrolls to the bottom when the keyboard is opened
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              // Display the question inside a container with padding and a border
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                child:Text(
-                  'Question',
-                  style: textStyle,
-                ),
+      body: Row(
+        children: [
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+                // Check if "Search" option is selected (assuming it's at index 1)
+                if (index == 1) {
+                  _navigateToSearch(); // Navigate to the search page
+                }
+              });
+            },
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
               ),
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Theme.of(context).colorScheme.primary),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  _defaultQuestion,
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
+              NavigationRailDestination(
+                icon: Icon(Icons.search),
+                label: Text('Search'),
               ),
-              const SizedBox(height: 20),
-              // Answer TextField
-              TextField(
-                controller: _answerController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your answer here',
-                ),
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-              ),
-              const SizedBox(height: 20),
-              // Align Submit Button to the right
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: _submitAnswer,
-                    child: const Text('Submit'),
-                  ),
-                ],
-              ),
+              // Add more NavigationRailDestinations as needed
             ],
           ),
-        ),
+          // Main content
+          Expanded(
+            child: SingleChildScrollView(
+              reverse: true,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        'Question',
+                        style: textStyle,
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Theme.of(context).colorScheme.primary),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(
+                        _defaultQuestion,
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      controller: _answerController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter your answer here',
+                      ),
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: _submitAnswer,
+                          child: const Text('Submit'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -111,3 +148,58 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 }
+
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BLE SCANNER"),
+        centerTitle: true,
+      ),
+      body: GetBuilder<BleController>(
+        init: BleController(),
+        builder: (controller) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                StreamBuilder<List<ScanResult>>(
+                    stream: controller.scanResults,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          shrinkWrap: true,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              final data = snapshot.data![index];
+                              return Card(
+                                elevation: 2,
+                                child: ListTile(
+                                  title: Text(data.device.name),
+                                  subtitle: Text(data.device.id.id),
+                                  trailing: Text(data.rssi.toString()),
+                                ),
+                              );
+                            });
+                      }else{
+                          return Center(child: Text("No Device Found"),);
+                      }
+                    }),
+                ElevatedButton(onPressed: () =>controller.scanDevices(), child: Text("Scan")),
+                SizedBox(
+                  height: 15,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
