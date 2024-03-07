320d636849bae2513d68521453fd0f3c806c93b6
github_username <feiyi@hawaii.edu>
2024-03-06 21:25:09 -1000

    flutter clean

--- BLE/build/macos/Build/Intermediates.noindex/Pods.build/Debug/flutter_blue_plus.build/Objects-normal/arm64/flutter_blue_plus_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/henrychen/Documents/GitHub/Quiz-App/build/macos/Build/Intermediates.noindex/Pods.build/Debug/flutter_blue_plus.build/module.modulemap \
-  /Users/henrychen/Documents/GitHub/Quiz-App/build/macos/Build/Intermediates.noindex/Pods.build/Debug/flutter_blue_plus.build/DerivedSources/flutter_blue_plus_vers.c
