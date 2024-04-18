d536b12f5d6cbba41b0e3985aeb011ad0e0ee500
Galen Chang <galenc12@gmail.com>
2024-04-18 12:23:54 -1000

    Update to Connected Devices Layout
    Small update to Connected Devices layout.

--- QuizApp-Instructor/lib/connected_devices_page.dart
+++ QuizApp-Instructor/lib/connected_devices_page.dart
@@ -9,24 +9,43 @@ class ConnectedDevicesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = Provider.of<MyAppState>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Connected Devices'),
-      ),
-      body: ListView.builder(
-        itemCount: appState.connectedDeviceIds.length, // Updated this line
-        itemBuilder: (context, index) {
-          String deviceId = appState.connectedDeviceIds[index]; // Updated this line
-          return ListTile(
-            title: Text(deviceId), // Display device identifier
-            trailing: ElevatedButton(
-              onPressed: () {
-                appState.disconnectFromDevice(deviceId);
-              },
-              child: const Text('Disconnect'),
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget> [
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            color: const Color.fromARGB(50, 6, 86, 6),
+            child: const Text(
+              'Connected Devices',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),  
             ),
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: appState.connectedDeviceIds.length, // Updated this line
+              itemBuilder: (context, index) {
+                String deviceId = appState.connectedDeviceIds[index]; // Updated this line
+                return ListTile(
+                  title: Text(deviceId), // Display device identifier
+                  trailing: ElevatedButton(
+                    onPressed: () {
+                      appState.disconnectFromDevice(deviceId);
+                    },
+                    child: const Text('Disconnect'),
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context,int index) {
+                  return const Divider();
+              }
+            ),
+          ),
+        ],
       ),
     );
   }
