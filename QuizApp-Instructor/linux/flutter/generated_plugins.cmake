038c9796b15785d409b00dc54f6b565419ef85ae
github_username <feiyi@hawaii.edu>
2024-04-18 15:18:12 -1000

    location permission for ios & macOS

--- QuizApp-Instructor/linux/flutter/generated_plugins.cmake
+++ QuizApp-Instructor/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  ble_peripheral
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- QuizApp-Instructor/windows/flutter/generated_plugin_registrant.cc
+++ QuizApp-Instructor/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <ble_peripheral/ble_peripheral_plugin_c_api.h>
-#include <quick_blue_windows/quick_blue_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  BlePeripheralPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("BlePeripheralPluginCApi"));
-  QuickBlueWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("QuickBlueWindowsPlugin"));
 }
