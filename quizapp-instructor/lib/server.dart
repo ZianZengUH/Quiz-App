b37b2f7c190b5bc9e15920a3bccf84ae37e482c5
ZianZengUH <zianzeng@hawaii.edu>
2024-04-30 01:01:36 -1000

    fix GPS verfication bug

--- quizapp-instructor/lib/server.dart
+++ quizapp-instructor/lib/server.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:quiz_app_instructor/main.dart';
 
 class Server extends ChangeNotifier {
   HttpServer? _server;
@@ -20,6 +21,8 @@ class Server extends ChangeNotifier {
     //    'Server running on IP: ${_server!.address.address}, Port: ${_server!.port}');
     //print("@@@@@@@@@\n");
 
+    var classroomLocation = await ClassroomLocationManager.getClassroomLocation();
+    
     for (var interface in await NetworkInterface.list()) {
       for (var addr in interface.addresses) {
         if (addr.type == InternetAddressType.IPv4 &&
@@ -42,6 +45,8 @@ class Server extends ChangeNotifier {
       if (WebSocketTransformer.isUpgradeRequest(request)) {
         // Handle WebSocket connection.
         WebSocket socket = await WebSocketTransformer.upgrade(request);
+        // Send the classroom location upon connection
+        socket.add(jsonEncode({'type': 'location', 'data': classroomLocation}));
         handleWebSocket(socket);
       } else {
         request.response
@@ -54,8 +59,8 @@ class Server extends ChangeNotifier {
   }
 
   void handleWebSocket(WebSocket socket) {
-    print('Client connected!');
     clients.add(socket);
+    print('Client connected!');
     socket.listen((data) async {
       onMessageReceived(data, socket);
     }, onDone: () {
@@ -77,9 +82,12 @@ class Server extends ChangeNotifier {
         String classSection = userData['classSection'].toString();
         String? photo = userData['photo'] as String?;
         String? answer = userData['answer'] as String?;
+        double studentLat = userData['latitude'];
+        double studentLong = userData['longitude'];
         print('Received Phone IP Address: $phoneIPAddress');
         manageStudentData(name, email, classSection, phoneIPAddress,
             photo: photo, answer: answer);
+        
       } else {
         print("Error: Received data is not as expected.");
       }
